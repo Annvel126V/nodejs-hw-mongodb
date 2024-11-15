075f8fe1b47d2d9cd3f02438b4f471378b3ab597
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-11-15 22:08:21 +0100

    add fix

--- src/index.js
+++ src/index.js
@@ -4,18 +4,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-async function start() {
-  try {
-    await initMongoConnection();
-    setupServer();
-  } catch (error) {
-    console.error('Error starting the application:', error);
-  }
-}
-start();
-console.log('MongoDB Environment Variables:', {
-  MONGODB_USER: process.env.MONGODB_USER,
-  MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
-  MONGODB_URL: process.env.MONGODB_URL,
-  MONGODB_DB: process.env.MONGODB_DB,
-});
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
--- src/server.js
+++ src/server.js
@@ -10,14 +10,20 @@ import {
 
 export function setupServer() {
   const app = express();
-  const logger = pino();
 
   app.use(cors());
-  app.use((req, res, next) => {
-    logger.info(`${req.method} ${req.url}`);
-    next();
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.get('/', async (req, res) => {
+    res.status(200).json({
+      message: 'Contact list',
+    });
   });
-
   app.get('/contacts', getAllContacts);
   app.get('/contacts/:contactId', getContactById);
 
